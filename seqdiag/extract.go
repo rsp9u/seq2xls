3b9bf57fff31a25e6559102e3ea53db35c7b4be1
rsp9u <gctwt852@yahoo.co.jp>
2019-05-21 19:01:54 +0900

    WIP Impl message extractor for seqdiag

--- seqdiag/extract.go
+++ seqdiag/extract.go
@@ -78,3 +78,57 @@ func containsLifeline(lls []*model.Lifeline, name string) bool {
 	}
 	return false
 }
+
+// ExtractMessages extracts message elements from the diagram.
+func ExtractMessages(d *ast.Diagram, lls []*model.Lifeline) ([]*model.Message, error) {
+	msgs := []*model.Message{}
+	msgs, _, err := extractMessagesFromStmts(d.Stmts.Items, lls, msgs, 0)
+	if err != nil {
+		return nil, err
+	}
+	return msgs, nil
+}
+
+func extractMessagesFromStmts(stmts []ast.Stmt, lls []*model.Lifeline, msgs []*model.Message, index int) ([]*model.Message, int, error) {
+	var (
+		indexCnt, indexPlus int
+		err                 error
+	)
+	indexCnt = 0
+
+	for _, stmt := range stmts {
+		switch v := stmt.(type) {
+		case *ast.FragmentStmt:
+		case *ast.GroupStmt:
+			msgs, indexPlus, err = extractMessagesFromStmts(v.Stmts.Items, lls, msgs, index)
+			if err != nil {
+				return nil, 0, err
+			}
+			index += indexPlus
+
+		case *ast.EdgeStmt:
+			for _, sgmt := range v.EdgeSegments.Items {
+				msg := &model.Message{
+					Index: index,
+					From: getLifeline(lls, sgmt.LeftNode.Value),
+					To: getLifeline(lls, sgmt.RightNode.Value),
+					Type: getMessageType(sgmt.Edge),
+					ColorHex: "000000",
+				}
+				msgs = append(msgs, msg)
+				index++
+				indexCnt++
+			}
+
+			if v.EdgeBlock != nil {
+				lls, indexPlus, err = extractLifelinesFromStmts(v.EdgeBlock.Items, lls, index)
+				if err != nil {
+					return nil, 0, err
+				}
+				index += indexPlus
+			}
+		}
+	}
+
+	return lls, indexCnt, nil
+}
