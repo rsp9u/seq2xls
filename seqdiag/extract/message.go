aac89fe29f8b08aa6aaca7a9b15b9b1fc17119d6
rsp9u <gctwt852@yahoo.co.jp>
2019-05-27 14:36:00 +0900

    Split 'seqdiag' extractor into multiple files

--- seqdiag/extract/message.go
+++ seqdiag/extract/message.go
@@ -1,4 +1,4 @@
-package seqdiag
+package extract
 
 import (
 	"strings"
@@ -7,81 +7,6 @@ import (
 	"github.com/rsp9u/seq2xls/model"
 	"github.com/rsp9u/seq2xls/seqdiag/ast"
 )
-
-// ExtractLifelines extracts lifeline elements from the diagram.
-func ExtractLifelines(d *ast.Diagram) ([]*model.Lifeline, error) {
-	lls := []*model.Lifeline{}
-	lls, _, err := extractLifelinesFromStmts(d.Stmts.Items, lls, 0)
-	if err != nil {
-		return nil, err
-	}
-	return lls, nil
-}
-
-func extractLifelinesFromStmts(stmts []ast.Stmt, lls []*model.Lifeline, index int) ([]*model.Lifeline, int, error) {
-	var (
-		indexCnt, indexPlus int
-		err                 error
-	)
-	indexCnt = 0
-
-	for _, stmt := range stmts {
-		switch v := stmt.(type) {
-		case ast.ContainerStmt:
-			lls, indexPlus, err = extractLifelinesFromStmts(v.GetItems(), lls, index)
-			if err != nil {
-				return nil, 0, err
-			}
-			index += indexPlus
-			indexCnt += indexPlus
-
-		case *ast.EdgeStmt:
-			for _, sgmt := range v.EdgeSegments.Items {
-				if !containsLifeline(lls, sgmt.LeftNode.Value) {
-					ll := &model.Lifeline{Name: sgmt.LeftNode.Value, Index: index, ColorHex: "FFFFFF"}
-					lls = append(lls, ll)
-					index++
-					indexCnt++
-				}
-
-				if !containsLifeline(lls, sgmt.RightNode.Value) {
-					ll := &model.Lifeline{Name: sgmt.RightNode.Value, Index: index, ColorHex: "FFFFFF"}
-					lls = append(lls, ll)
-					index++
-					indexCnt++
-				}
-			}
-
-			if v.EdgeBlock != nil {
-				lls, indexPlus, err = extractLifelinesFromStmts(v.EdgeBlock.Items, lls, index)
-				if err != nil {
-					return nil, 0, err
-				}
-				index += indexPlus
-				indexCnt += indexPlus
-			}
-
-		case *ast.NodeStmt:
-			if !containsLifeline(lls, v.ID.Value) {
-				ll := &model.Lifeline{Name: v.ID.Value, Index: index, ColorHex: "FFFFFF"}
-				lls = append(lls, ll)
-				index++
-				indexCnt++
-			}
-		}
-	}
-
-	return lls, indexCnt, nil
-}
-
-func containsLifeline(lls []*model.Lifeline, name string) bool {
-	for _, ll := range lls {
-		if ll.Name == name {
-			return true
-		}
-	}
-	return false
-}
 
 // ExtractMessages extracts message elements from the diagram.
 func ExtractMessages(d *ast.Diagram, lls []*model.Lifeline) ([]*model.Message, []*model.Note, error) {
--- seqdiag/extract/message_test.go
+++ seqdiag/extract/message_test.go
@@ -6,49 +6,6 @@ import (
 	"github.com/rsp9u/seq2xls/model"
 )
 
-const testDataLifeline = `
-seqdiag {
-  webserver; database; browser;
-  browser  -> webserver [label = "GET /index.html"];
-  browser <-- webserver;
-  group {
-    foo; bar;
-  }
-  loop {
-    baz => qux; 
-  }
-}
-`
-
-func checkLifeline(t *testing.T, ll *model.Lifeline, idx int, name string) {
-	if ll.Index != idx {
-		t.Fatalf("Invalid lifeline index %s[%d]", ll.Name, ll.Index)
-	}
-	if ll.Name != name {
-		t.Fatalf("Mismatches lifeline name [expect: %s, actual: %s]", name, ll.Name)
-	}
-}
-
-func TestExtractLifelines(t *testing.T) {
-	d := ParseSeqdiag([]byte(testDataLifeline))
-	lls, err := ExtractLifelines(d)
-	if err != nil {
-		t.Fatalf("Extract error %v", err)
-	}
-
-	if len(lls) > 7 {
-		t.Fatalf("Too many lifelines %d", len(lls))
-	}
-
-	checkLifeline(t, lls[0], 0, "webserver")
-	checkLifeline(t, lls[1], 1, "database")
-	checkLifeline(t, lls[2], 2, "browser")
-	checkLifeline(t, lls[3], 3, "foo")
-	checkLifeline(t, lls[4], 4, "bar")
-	checkLifeline(t, lls[5], 5, "baz")
-	checkLifeline(t, lls[6], 6, "qux")
-}
-
 const testDataMessage = `
 seqdiag {
   foo  -> bar;
